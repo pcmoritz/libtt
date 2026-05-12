a6c0bdc362a9bf70c0e73173f928265d95c5d7c6
Philipp Moritz <pcmoritz@gmail.com>
2026-05-11 17:07:15 -0700

    Implement power kernel

---
+++ kernels/power_compute.cc
@@ -0,0 +1,40 @@
+#include <cstdint>
+#include "compute_kernel_api/common.h"
+#include "compute_kernel_api/tile_move_copy.h"
+#include "compute_kernel_api/eltwise_unary/eltwise_unary.h"
+#include "compute_kernel_api/eltwise_binary_sfpu.h"
+#include "compute_kernel_api.h"
+
+namespace NAMESPACE {
+void MAIN {
+  uint32_t n_tiles = get_arg_val<uint32_t>(0);
+  constexpr uint32_t cb_lhs = tt::CBIndex::c_0;
+  constexpr uint32_t cb_rhs = tt::CBIndex::c_1;
+  constexpr uint32_t cb_out = tt::CBIndex::c_16;
+
+  unary_op_init_common(cb_lhs, cb_out);
+  power_binary_tile_init();
+
+  for (uint32_t i = 0; i < n_tiles; ++i) {
+    cb_wait_front(cb_lhs, 1);
+    cb_wait_front(cb_rhs, 1);
+    cb_reserve_back(cb_out, 1);
+
+    tile_regs_acquire();
+    copy_tile_to_dst_init_short_with_dt(cb_rhs, cb_lhs);
+    copy_tile(cb_lhs, 0, 0);
+    copy_tile_to_dst_init_short_with_dt(cb_lhs, cb_rhs);
+    copy_tile(cb_rhs, 0, 1);
+    power_binary_tile(0, 1, 0);
+    tile_regs_commit();
+
+    tile_regs_wait();
+    pack_tile(0, cb_out);
+    tile_regs_release();
+
+    cb_pop_front(cb_lhs, 1);
+    cb_pop_front(cb_rhs, 1);
+    cb_push_back(cb_out, 1);
+  }
+}
+}  // namespace NAMESPACE
