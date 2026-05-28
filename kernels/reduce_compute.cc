603937f1d2ff1376274cc83ae36a46fe456ff234
Philipp Moritz <pcmoritz@gmail.com>
2026-05-27 18:14:59 -0700

    update

--- kernels/reduce_compute.cc
+++ kernels/reduce_compute.cc
@@ -12,44 +12,42 @@
 
 namespace NAMESPACE {
 
-template <DataFormat Format>
 void add_reduce_init() {
-  if constexpr (Format == DataFormat::Float16 || Format == DataFormat::Float16_b ||
-                Format == DataFormat::Float32) {
+  if constexpr (REDUCE_DATA_FORMAT == DataFormat::Float16 ||
+                REDUCE_DATA_FORMAT == DataFormat::Float16_b ||
+                REDUCE_DATA_FORMAT == DataFormat::Float32) {
     add_binary_tile_init();
   } else {
     ckernel::add_int_tile_init();
   }
 }
 
-template <DataFormat Format>
 void add_reduce_tile(uint32_t lhs, uint32_t rhs, uint32_t output) {
-  if constexpr (Format == DataFormat::Float16 || Format == DataFormat::Float16_b ||
-                Format == DataFormat::Float32) {
+  if constexpr (REDUCE_DATA_FORMAT == DataFormat::Float16 ||
+                REDUCE_DATA_FORMAT == DataFormat::Float16_b ||
+                REDUCE_DATA_FORMAT == DataFormat::Float32) {
     add_binary_tile(lhs, rhs, output);
-  } else if constexpr (Format == DataFormat::Int32) {
+  } else if constexpr (REDUCE_DATA_FORMAT == DataFormat::Int32) {
     ckernel::add_int32_tile(lhs, rhs, output);
-  } else if constexpr (Format == DataFormat::UInt32) {
+  } else if constexpr (REDUCE_DATA_FORMAT == DataFormat::UInt32) {
     ckernel::add_uint32_tile(lhs, rhs, output);
-  } else if constexpr (Format == DataFormat::UInt16) {
+  } else if constexpr (REDUCE_DATA_FORMAT == DataFormat::UInt16) {
     ckernel::add_uint16_tile(lhs, rhs, output);
   }
 }
 
-template <DataFormat Format>
 void min_reduce_tile(uint32_t lhs, uint32_t rhs, uint32_t output) {
   ckernel::binary_min_tile_init();
-  if constexpr (Format == DataFormat::Int32) {
+  if constexpr (REDUCE_DATA_FORMAT == DataFormat::Int32) {
     ckernel::binary_min_int32_tile(lhs, rhs, output);
   } else {
     ckernel::binary_min_tile(lhs, rhs, output);
   }
 }
 
-template <DataFormat Format>
 void max_reduce_tile(uint32_t lhs, uint32_t rhs, uint32_t output) {
   ckernel::binary_max_tile_init();
-  if constexpr (Format == DataFormat::Int32) {
+  if constexpr (REDUCE_DATA_FORMAT == DataFormat::Int32) {
     ckernel::binary_max_int32_tile(lhs, rhs, output);
   } else {
     ckernel::binary_max_tile(lhs, rhs, output);
@@ -68,17 +66,16 @@ void reduce_last_dim_tile(uint32_t dst_idx) {
 }
 #endif
 
-template <DataFormat Format>
 void bitwise_reduce_tile(uint32_t lhs, uint32_t rhs, uint32_t output) {
   ckernel::binary_bitwise_tile_init();
 #if REDUCE_IS_OR
-  if constexpr (Format == DataFormat::UInt16) {
+  if constexpr (REDUCE_DATA_FORMAT == DataFormat::UInt16) {
     ckernel::bitwise_or_uint16_binary_tile(lhs, rhs, output);
   } else {
     ckernel::bitwise_or_uint32_binary_tile(lhs, rhs, output);
   }
 #else
-  if constexpr (Format == DataFormat::UInt16) {
+  if constexpr (REDUCE_DATA_FORMAT == DataFormat::UInt16) {
     ckernel::bitwise_and_uint16_binary_tile(lhs, rhs, output);
   } else {
     ckernel::bitwise_and_uint32_binary_tile(lhs, rhs, output);
@@ -86,20 +83,19 @@ void bitwise_reduce_tile(uint32_t lhs, uint32_t rhs, uint32_t output) {
 #endif
 }
 
-template <DataFormat Format>
 void combine_into_accumulator(uint32_t index, uint32_t dst_idx) {
   if (index == 0) {
     return;
   }
 #if REDUCE_IS_BITWISE
-  bitwise_reduce_tile<Format>(0, dst_idx, 0);
+  bitwise_reduce_tile(0, dst_idx, 0);
 #elif REDUCE_IS_SUM
-  add_reduce_init<Format>();
-  add_reduce_tile<Format>(0, dst_idx, 0);
+  add_reduce_init();
+  add_reduce_tile(0, dst_idx, 0);
 #elif REDUCE_IS_MIN
-  min_reduce_tile<Format>(0, dst_idx, 0);
+  min_reduce_tile(0, dst_idx, 0);
 #else
-  max_reduce_tile<Format>(0, dst_idx, 0);
+  max_reduce_tile(0, dst_idx, 0);
 #endif
 }
 
@@ -130,7 +126,7 @@ void MAIN {
         cb_wait_front(cb_input, REDUCE_BLOCK_MAX_ROW_TILES);
         ckernel::reduce_block_max_row<REDUCE_BLOCK_MAX_ROW_TILES>(
             cb_input, cb_scaler, 0, dst_idx);
-        combine_into_accumulator<REDUCE_DATA_FORMAT>(block, dst_idx);
+        combine_into_accumulator(block, dst_idx);
         cb_pop_front(cb_input, REDUCE_BLOCK_MAX_ROW_TILES);
       }
       cb_reserve_back(cb_output, onetile);
@@ -152,7 +148,7 @@ void MAIN {
         copy_tile_to_dst_init_short(cb_input);
         copy_tile(cb_input, 0, dst_idx);
         reduce_last_dim_tile(dst_idx);
-        combine_into_accumulator<REDUCE_DATA_FORMAT>(wt, dst_idx);
+        combine_into_accumulator(wt, dst_idx);
         cb_pop_front(cb_input, onetile);
       }
       cb_reserve_back(cb_output, onetile);
@@ -175,7 +171,7 @@ void MAIN {
       cb_wait_front(cb_input, onetile);
       copy_tile_to_dst_init_short(cb_input);
       copy_tile(cb_input, 0, dst_idx);
-      combine_into_accumulator<REDUCE_DATA_FORMAT>(index, dst_idx);
+      combine_into_accumulator(index, dst_idx);
       cb_pop_front(cb_input, onetile);
     }
     cb_reserve_back(cb_output, onetile);
