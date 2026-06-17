4ace4f31f7b5f8b2bc27c1dc01546c8383b41f25
Philipp Moritz <pcmoritz@gmail.com>
2026-06-16 19:17:33 -0700

    Remove local PJRT runtime implementation (#98)
    Remove the old separate implementation.

--- cpp/libtt_pjrt.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBTT_CPP_LIBTT_PJRT_H_
-#define LIBTT_CPP_LIBTT_PJRT_H_
-
-#include "xla/pjrt/c/pjrt_c_api.h"
-
-extern "C" const PJRT_Api* GetPjrtApi();
-
-#endif  // LIBTT_CPP_LIBTT_PJRT_H_
