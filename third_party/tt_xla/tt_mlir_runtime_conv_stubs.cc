9df46c92c496eae1039444b4bb5f2e9bac760c84
Philipp Moritz <pcmoritz@gmail.com>
2026-08-05 17:55:14 -0700

    Fix a number of convolution tests (#168)
    This can be tested with
    
      bazel test //tests:jax_test_suite \
        --test_output=streamed \
        --nocache_test_results \
        --test_timeout=43200 \
        --test_arg=tests/lax_autodiff_test.py::LaxAutodiffTest::testConvGeneralDilatedGrad9 \
        --test_arg=tests/lax_autodiff_test.py::LaxAutodiffTest::testConvGrad{0,1,2,3,5,8} \
        --test_arg=tests/lax_autodiff_test.py::LaxAutodiffTest::testConvWithGeneralPaddingGrad{2,6} \
        --test_arg=tests/lax_test.py::LaxTest::testConv{0..9} \
        --test_arg=tests/lax_test.py::LaxTest::testConvAgainstNumpy{0..9} \
        --test_arg=tests/lax_test.py::LaxTest::testConvGeneralDilated{2,4,8,9} \
        --test_arg=tests/lax_test.py::LaxTest::testConvGeneralDilatedLocal{0,1,4,6,7,8} \
        --test_arg=tests/lax_test.py::LaxTest::testConvGeneralDilatedPatchesNonOverlapping6 \
        --test_arg=tests/lax_test.py::LaxTest::testConvGeneralDilatedPatchesOverlapping2D \
        --test_arg=tests/lax_test.py::LaxTest::testConvTranspose1D{0..9} \
        --test_arg=tests/lax_test.py::LaxTest::testConvTranspose2D{0..9} \
        --test_arg=tests/lax_test.py::LaxTest::testConvTranspose2DT{0..9} \
        --test_arg=tests/lax_test.py::LaxTest::testConvTransposePaddingList \
        --test_arg=tests/lax_test.py::LaxTest::testConvWithGeneralPadding{0,1,5,6,9} \
        --test_arg=tests/lax_test.py::LaxTest::testConvWithGeneralPaddingAgainstNumpy{3,4,7} \
        --test_arg=tests/lax_vmap_test.py::LaxVmapTest::testConvGeneralDilatedBatching{1,2,6,8} \
        --test_arg=--override-ini=addopts= \
        --test_arg=-p \
        --test_arg=no:faulthandler \
        --test_arg=-p \
        --test_arg=no:benchmark \
        --test_arg=--tb=no

--- third_party/tt_xla/tt_mlir_runtime_conv_stubs.cc
+++ third_party/tt_xla/tt_mlir_runtime_conv_stubs.cc
@@ -1,14 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
-#include "operations/conv/conv1d.h"
 #include "operations/conv/conv3d.h"
-#include "operations/conv/conv2d.h"
-#include "operations/conv/conv_transpose2d.h"
-#include "operations/conv/prepare_conv2d_bias.h"
-#include "operations/conv/prepare_conv2d_weights.h"
 #include "operations/conv/prepare_conv3d_weights.h"
-#include "operations/conv/prepare_conv_transpose2d_bias.h"
-#include "operations/conv/prepare_conv_transpose2d_weights.h"
 
 #include <stdexcept>
 #include <string>
@@ -21,43 +14,12 @@ namespace {
 }
 } // namespace
 
-void run(const ::tt::target::ttnn::Conv1dOp *, ProgramContext &) {
-  unsupported("ttnn Conv1dOp");
-}
-
-void run(const ::tt::target::ttnn::Conv2dOp *, ProgramContext &) {
-  unsupported("ttnn Conv2dOp");
-}
-
 void run(const ::tt::target::ttnn::Conv3dOp *, ProgramContext &) {
   unsupported("ttnn Conv3dOp");
-}
-
-void run(const ::tt::target::ttnn::ConvTranspose2dOp *, ProgramContext &) {
-  unsupported("ttnn ConvTranspose2dOp");
-}
-
-void run(const ::tt::target::ttnn::PrepareConv2dBiasOp *, ProgramContext &) {
-  unsupported("ttnn PrepareConv2dBiasOp");
-}
-
-void run(const ::tt::target::ttnn::PrepareConv2dWeightsOp *,
-         ProgramContext &) {
-  unsupported("ttnn PrepareConv2dWeightsOp");
 }
 
 void run(const ::tt::target::ttnn::PrepareConv3dWeightsOp *,
          ProgramContext &) {
   unsupported("ttnn PrepareConv3dWeightsOp");
-}
-
-void run(const ::tt::target::ttnn::PrepareConvTranspose2dBiasOp *,
-         ProgramContext &) {
-  unsupported("ttnn PrepareConvTranspose2dBiasOp");
-}
-
-void run(const ::tt::target::ttnn::PrepareConvTranspose2dWeightsOp *,
-         ProgramContext &) {
-  unsupported("ttnn PrepareConvTranspose2dWeightsOp");
 }
 } // namespace tt::runtime::ttnn::operations::conv
